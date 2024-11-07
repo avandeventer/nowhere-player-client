c8bb81a709840ab5ad7653443002e3bce63b7951
avandeventer <vandeventerandy@gmail.com>
2024-11-07 14:08:51 -0500

    Fixing bugs. Adding Favor to player stats

--- src/assets/player.ts
+++ src/assets/player.ts
@@ -1,21 +1,23 @@
 export class Player {
-  name: string;
+  userName: string;
   strength: number;
   intellect: number;
   charisma: number;
   dexterity: number;
   wealth: number;
   magic: number;
+  favor: number;
   authorId: string;
 
   constructor() {
-    this.name = '';
+    this.userName = '';
     this.strength = 0;
     this.intellect = 0;
     this.charisma = 0;
     this.dexterity = 0;
     this.wealth = 0;
     this.magic = 0;
+    this.favor = 0;
     this.authorId = "";
   }
 }
